81064c170ef362dfe239584c57f15c600771c087
Atharv-28 <atharvmt@outlook.com>
2024-06-19 02:08:54 +0530

    Final commit

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,15 +1,69 @@
 const questions = [
-    {
-      question: "What is the capital of France?",
-      options: ["Paris", "London", "Berlin", "Madrid"],
-      answer: "Paris"
-    },
-    {
-      question: "What is 2 + 2?",
-      options: ["3", "4", "5", "6"],
-      answer: "4"
-    },
-  ];
-  
-  export default questions;
-  
+  {
+    question: "Who won the 2024 24 hours of Le Mans in Hyper Car category?",
+    options: ["Alpine", "Ferrari", "Toyota", "Porsche"],
+    answer: "Ferrari",
+  },
+  {
+    question: "Who is current reining World Champion in F1?",
+    options: [
+      "Fernando Alonso",
+      "Lewis Hamilton",
+      "Max Verstappen",
+      "Charles Leclerc",
+    ],
+    answer: "Max Verstappen",
+  },
+  {
+    question: "Which of the following is not a Triple Crown Race?",
+    options: ["Monaco GP", "24h of Le Mans", "Monza GP", "Indianopolis 500"],
+    answer: "Monza GP",
+  },
+  {
+    question: "Who won the 2024 Monaco GP?",
+    options: ["Alpine", "Ferrari", "McLaren", "RedBull"],
+    answer: "Ferrari",
+  },
+  {
+    question: "Where F1 tests new cars aka Pre Season Testing?",
+    options: ["Silverstone", "Australia", "Barcelona", "Bahrain"],
+    answer: "Bahrain",
+  },
+  {
+    question: "Which driver has no. 16 on current F1 grid?",
+    options: [
+        "Fernando Alonso",
+        "Lewis Hamilton",
+        "Max Verstappen",
+        "Charles Leclerc",
+      ],
+      answer: "Charles Leclerc",
+  },
+  {
+    question: "Which of following driver is not multiple grand prix winner?",
+    options: [
+        "Carlos Sainz",
+        "Lando Norris",
+        "Max Verstappen",
+        "Charles Leclerc",
+      ],
+      answer: "Lando Norris",
+  },
+  {
+    question: "Which Team is currently leading Constructors Championship in F1?",
+    options: ["Mercedes", "Ferrari", "McLaren", "Red Bull"],
+    answer: "Red Bull",
+  },
+  {
+    question: "Which Team is currently leading Manufacturing Championship in WEC?",
+    options: ["Alpine", "Ferrari", "Toyota", "Porsche"],
+    answer: "Porsche",
+  },
+  {
+    question: "Full Form of F1?",
+    options: ["Fast1", "Ferrari1", "Formula1", "Forever1"],
+    answer: "Formula1",
+  },
+];
+
+export default questions;
